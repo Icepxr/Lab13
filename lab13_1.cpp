52f27423fc874e1034ee6ad9f4a38e724e551508
Daiyaz <uchihachikk3@gmail.com>
2024-12-27 15:39:10 +0700

    Add statistical functions to calculate mean, standard deviation, geometric mean, harmonic mean, and min/max values

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,36 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double x[],int n,double y[]){
+    double sum=0;
+    double A=x[0];
+    double B=x[0];
+    for(int i=0;i<n;i++){
+        sum += x[i];
+    }
+    y[0] = sum/n;
+    sum =0;
+    for(int i=0;i<n;i++){
+        sum += pow(x[i]-y[0],2);
+    }
+    sum = sqrt((1.00/n)*sum);
+    y[1] = sum;
+    sum=1;
+    for(int i=0;i<n;i++){
+        sum *=x[i];
+    }
+    sum = pow(sum,1.00/n);
+    y[2] = sum;
+    sum =0;
+    for(int i=0;i<n;i++){
+        sum +=1/x[i];
+    }
+    y[3] = n/sum;
+    for(int i=0;i<n;i++){
+        if(A>x[i]) A=x[i];
+        if(B<x[i]) B =x[i];
+    }
+    y[4] = B;
+    y[5] = A;
+}
